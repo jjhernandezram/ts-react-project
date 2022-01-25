4fb0cf353e2fe6baf4b9b963d87874e03a844e3a
jjhernandezram <jjhernandezram@gmail.com>
2022-01-25 11:53:47 -0600

    update branch

--- src/lazyload/layout/LazyLayout.tsx
+++ src/lazyload/layout/LazyLayout.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import { LazyPage1, LazyPage2, LazyPage3 } from '../pages';
 
 export const LazyLayout = () => {
@@ -21,6 +21,7 @@ export const LazyLayout = () => {
         <Route path='lazy1' element={<LazyPage1 />} />
         <Route path='lazy2' element={<LazyPage2 />} />
         <Route path='lazy3' element={<LazyPage3 />} />
+        <Route path='*' element={<Navigate to='lazy1' replace />} />
       </Routes>
     </div>
   );
